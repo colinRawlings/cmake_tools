f887cdc9a090d3be7e83f4aadc5833734627f5ca
Colin Rawlings <colinrawlings@Colins-MacBook-Pro-2.local>
2019-05-19 20:03:26 +0200

    Further extensions to clang format specification

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,40 +3,28 @@
 
 #include <a_lib.h>
 
-int my_function(
-    int arg, int arg2,
-    int arg_with_a_very_long_name,
-    int arg4)
+int my_function(int arg, int arg2, int arg_with_a_very_long_name, int arg4)
 {
   return 0;
 }
 
 int main()
 {
-  std::cout << "called: "
-            << __FUNCTION__
-            << std::endl;
+  std::cout << "called: " << __FUNCTION__ << std::endl;
 
-  int a_long_variable_name =
-      get_num_liba();
+  int a_long_variable_name = get_num_liba();
 
-  int a =
-      my_function(a_long_variable_name,
-                  a_long_variable_name,
-                  a_long_variable_name,
-                  a_long_variable_name);
+  int b = my_function(a_long_variable_name, a_long_variable_name,
+                      a_long_variable_name, a_long_variable_name);
 
-  int a =
-      my_function(a_long_variable_name,
-                  a_long_variable_name,
-                  a_long_variable_name,
-                  a_long_variable_name);
+  int c = my_function(a_long_variable_name, a_long_variable_name,
+                      a_long_variable_name, a_long_variable_name);
 
   std::string a_string("hello");
 
   std::cout << a_string << std::endl;
 
-  int b(a);
+  int d(b);
 
   return 0;
 }
--- lib_a/src/a_lib.cpp
+++ lib_a/src/a_lib.cpp
@@ -8,3 +8,33 @@ int get_num_liba()
 
     return 42;
 }
+
+// zmt style guide cases
+
+// STYLEGUIDE: Enumeration Names
+enum eResult { kNone, kAll, kSomeAvailable };
+
+// STYLEGUIDE: (???) namespaces should be nested on a single line with a single
+// indent
+namespace XTest { namespace XNest {
+
+    bool IsThatAwesome() { return true; }
+
+    // STYLEGUIDE: Curly Braces
+    bool IsThatFuncAwesome(bool condition, bool another_condition)
+    {
+        if (condition)
+        {
+            if (another_condition)
+            {
+            }
+        }
+
+        return false;
+    }
+}} // namespace XTest::XNest
+
+// STYLEGUIDE: Class Member Initialisation
+
+CMyClass::CMyClass()
+    : m_MyVariable(1.23), m_MyVariable2(1.17), m_MyVariable3(3.21){};
