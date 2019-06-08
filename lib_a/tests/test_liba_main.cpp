fb45dfdde71bdd3a90d204f3ec3c764596cc91ef
colinRawlings <colin.d.rawlings@gmail.com>
2019-06-08 10:10:50 +0200

    Ninja build works, clean outdir specification and automated symbol export disabled

--- lib_a/tests/test_liba_main.cpp
+++ lib_a/tests/test_liba_main.cpp
@@ -1,3 +1,10 @@
+#define BOOST_TEST_MODULE testsuite_liba
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE liba_test_module
+#define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
+
+// entry point:
+int __declspec(dllexport) main(int argc, char *argv[], char *envp[])
+{
+    return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
+}
